e739f8ecdfd4daacf0a660dfdbc326f7de368306
Nicolas Ruiz <syngenruiz2@gmail.com>
2022-04-24 01:14:41 -0500

    Shows data from firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { db } from './firebase';
+import { collection } from 'firebase/firestore';
+import { getDocs } from 'firebase/firestore';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Username: {user.username}</h1>
+            <h1>Password: {user.password}</h1>
+          </div>
+        );
+      })};
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC8qk6W9fqT30UrbCqAHuBHzGDbpnCKCJo",
+    authDomain: "hackunt-solo-challenge.firebaseapp.com",
+    projectId: "hackunt-solo-challenge",
+    storageBucket: "hackunt-solo-challenge.appspot.com",
+    messagingSenderId: "561542156717",
+    appId: "1:561542156717:web:91251ccc50bc8d74a6d124",
+    measurementId: "G-QVTVYHZGH1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
