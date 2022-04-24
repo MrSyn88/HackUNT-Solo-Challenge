7d6511237662437f8efd6011a5df8fe8ad33b41a
Nicolas Ruiz <syngenruiz2@gmail.com>
2022-04-24 03:32:36 -0500

    finished create and check user with password

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { db } from './firebase';
-import { collection } from 'firebase/firestore';
-import { getDocs } from 'firebase/firestore';
+import { addDoc, collection } from 'firebase/firestore';
+import { getDocs, query, where } from 'firebase/firestore';
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newPass, setNewPass] = useState("");
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { username: newName, password: newPass });
+  }
+
+  const checkUser = async () => {
+    const q = query(collection(db, "users"), where("username", "==", newName));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      alert("Not and existing user");
+    }
+
+    querySnapshot.forEach((doc) => {
+      if (doc.get("password") == newPass) {
+        alert("User found!");
+      }
+      else {
+        alert("Password incorrect");
+      }
+    });
+  }
 
   useEffect(() => {
 
@@ -21,14 +45,15 @@ function App() {
 
   return (
     <div className="App">
-      {users.map((user) => {
-        return (
-          <div>
-            <h1>Username: {user.username}</h1>
-            <h1>Password: {user.password}</h1>
-          </div>
-        );
-      })};
+      <div>
+        <input placeholder='Username' onChange={(event) => { setNewName(event.target.value) }} />
+        <input placeholder='Password' onChange={(event) => { setNewPass(event.target.value) }} />
+      </div>
+      <div>
+        <button onClick={createUser}>Create User</button>
+        <button onClick={checkUser}>Check User</button>
+      </div>
+
     </div>
   );
 }
